4e38103fcfd5566dbb47df97ebc0d6892de3d073
Sourav Chatterjee <sromic@gmail.com>
2024-09-29 19:10:01 +0300

    Making the screen interpolate between various colors by using varying values.

--- 1.FirstShader/shaders/fragment-shader.glsl
+++ 1.FirstShader/shaders/fragment-shader.glsl
@@ -1,4 +1,7 @@
+varying vec2 vUvs;
+
 void main()
 {
-    gl_FragColor = vec4(1.0,0.0, 0.0, 1.0);
+    //gl_FragColor = vec4(vUvs, 0.0, 1.0);
+    gl_FragColor = vec4(1.0 - vUvs.x, 0.0, 1.0 - vUvs.y, 1.0);
 }
